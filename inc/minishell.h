0ae1102cfa6ca598cf9839b27d1cb0688f4300c8
Arthur <arthurascedusnkrs@gmail.com>
2023-03-30 15:21:03 +0200

    Fixing Makefile recompilation

---
+++ inc/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arthurascedu <arthurascedu@student.42ly    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 14:41:00 by arthurasced       #+#    #+#             */
+/*   Updated: 2023/03/30 15:05:47 by arthurasced      ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include "../libft/header/libft.h"
+
+#endif
