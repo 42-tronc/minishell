77a0942915d711918167d4145e3081f1d7726057
Maplepy <croy@student.42lyon.fr>
2023-03-31 13:06:59 +0200

    add no_newline function

--- src/exec/built-in/echo.c
+++ src/exec/built-in/echo.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:41:40 by croy              #+#    #+#             */
-/*   Updated: 2023/03/30 16:41:20 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/03/31 12:57:52 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,28 @@ X	->	print with \n
 	--n non
  */
 
+int	no_newline(char *str)
+{
+	int	i;
+
+	i = 1;
+	if (str[0] == '-')
+	{
+		while (str[i] && str[i] == 'n')
+			i++;
+	}
+	if (!str[i])
+		return (1);
+	return (0);
+
+}
+
 int	main(void)
 {
 	printf("cc bg\n");
-	return 0;
+	if (no_newline())
+		printf("loli");
+	else
+		printf("loli\n");
+	return (0);
 }
