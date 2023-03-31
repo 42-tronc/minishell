97b39796ae0e9c2f8aa69aec28fde85d25a61285
Maplepy <croy@student.42lyon.fr>
2023-03-31 16:12:16 +0200

    fix echo with -n

--- src/exec/built-in/echo.c
+++ src/exec/built-in/echo.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:41:40 by croy              #+#    #+#             */
-/*   Updated: 2023/03/31 12:57:52 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/03/31 16:11:26 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,43 @@
 
 X	->	print with \n
 -n	->	print without \n
-	--n non
+-nn ->	without \n
+--n ->	text
  */
 
-int	no_newline(char *str)
+int	need_newline(char *str)
 {
 	int	i;
 
 	i = 1;
-	if (str[0] == '-')
+	if (str && str[0] == '-')
 	{
 		while (str[i] && str[i] == 'n')
 			i++;
+		if (str[i] && str[i] != 'n')
+		{
+			printf("not an 'n'\n");
+			return (1);
+		}
 	}
-	if (!str[i])
+	else
 		return (1);
 	return (0);
-
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	printf("cc bg\n");
-	if (no_newline())
-		printf("loli");
-	else
-		printf("loli\n");
+	// if (ac >= 3)
+	// {
+	// 	printf("cc bg\n");
+	// 	// if (need_newline())
+	// 		printf("loli");
+	// 	else
+	// 		printf("loli\n");
+	// 	return (0);
+	// }
+	(void) ac;
+	printf("arg = '%s'\n", av[1]);
+	printf("Need new line = %d\n", need_newline(av[1]));
 	return (0);
 }
