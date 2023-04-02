cbe014e70f400064a47a79abd240bfdec6817cb2
maplepy <github.register@slmail.me>
2023-04-02 20:01:51 +0200

    fix echo

--- src/exec/built-in/echo.c
+++ src/exec/built-in/echo.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:41:40 by croy              #+#    #+#             */
-/*   Updated: 2023/03/31 16:27:29 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/02 20:01:29 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,27 @@ int	need_newline(char *str)
 	int	i;
 
 	i = 1;
+	if (!str)
+		return (1);
 	if (str && str[0] == '-')
 	{
-		if (str && str[1] != 'n')
-			return (1);
-		while (str[i] && str[i] == 'n')
+		if (str && str[1] == 'n')
+		{
 			i++;
-		if (str[i] && str[i] != 'n')
-			return (1);
+			while (str[i] && str[i] == 'n')
+				i++;
+			if (!str[i])
+				return (0);
+		}
 	}
-	else
-		return (1);
-	return (0);
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac >= 2)
 	{
+		printf("param= '%s'\n", av[1]);
 		printf("Need new line = %d\n", need_newline(av[1]));
 		if (need_newline(av[1]))
 			printf("loli\n");
