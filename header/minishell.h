c32bf4435572f4e566587e7f4bf10d184334a44a
maplepy <github.register@slmail.me>
2023-04-03 17:16:21 +0200

    make use of -I to include libft.h

--- header/minishell.h
+++ header/minishell.h
@@ -6,14 +6,14 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:55:43 by croy              #+#    #+#             */
-/*   Updated: 2023/04/03 17:13:29 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 17:16:02 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/header/libft.h"
+# include "libft.h"
 # include "exec.h"
 // # include "parsing.h"
 
