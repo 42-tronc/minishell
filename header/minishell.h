ed4937f436780f6259f64397e8a294f217222932
maplepy <github.register@slmail.me>
2023-04-03 17:08:40 +0200

    add clean header with both exec and parsing

--- header/minishell.h
+++ header/minishell.h
@@ -6,14 +6,17 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:55:43 by croy              #+#    #+#             */
-/*   Updated: 2023/04/03 16:57:10 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 17:08:07 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+# include "../libft/header/libft.h"
+# include "exec.h"
+# include "parsing.h"
+
 # include <stdio.h>
 
 #endif
