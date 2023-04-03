4603cc7270752188898c75d0defce92e2d6cc35c
maplepy <github.register@slmail.me>
2023-04-03 16:58:15 +0200

    fix exec header

--- header/exec.h
+++ header/exec.h
@@ -6,14 +6,17 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:26:14 by croy              #+#    #+#             */
-/*   Updated: 2023/04/03 15:28:15 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 16:56:09 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
+# include "minishell.h"
+
 # include <limits.h>
 # include <unistd.h>
+// # include <stdio.h>
 
 #endif
