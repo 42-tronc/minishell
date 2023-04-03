830a9b57d010a6010808c77d9a95452143e0c188
maplepy <github.register@slmail.me>
2023-04-03 16:57:36 +0200

    fix minishell header

--- header/minishell.h
+++ header/minishell.h
@@ -6,17 +6,14 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:55:43 by croy              #+#    #+#             */
-/*   Updated: 2023/04/03 15:28:10 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 16:57:10 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/header/libft.h"
-# include "parsing.h"
-# include "exec.h"
-
+# include "libft.h"
 # include <stdio.h>
 
 #endif
