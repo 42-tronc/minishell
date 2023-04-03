9575adf812224331e308c1dff3bb43404e2b8f83
Arthur <arthurascedusnkrs@gmail.com>
2023-04-03 14:43:51 +0200

    WOUF CORRECTED

--- header/parsing.h
+++ header/parsing.h
@@ -6,7 +6,7 @@
 /*   By: arthurascedu <arthurascedu@student.42ly    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 11:13:53 by arthurasced       #+#    #+#             */
-/*   Updated: 2023/04/03 13:49:48 by arthurasced      ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 14:16:32 by arthurasced      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ struct s_token {
 	t_token	*next;
 };
 
-void	getting_line(t_token *tokens);
+t_token	*getting_line(void);
 void	ft_tokenadd_back(t_token **lst, t_token *new);
 t_token	*ft_tokennew(void *content);
 void	add_token(t_token **tokens, char *str, int begin, int end);
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,29 +6,27 @@
 /*   By: arthurascedu <arthurascedu@student.42ly    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 10:19:54 by arthurasced       #+#    #+#             */
-/*   Updated: 2023/04/03 13:50:01 by arthurasced      ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 14:18:52 by arthurasced      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	print_tokens_linked_list(t_token* head) {
-// 	t_token *temp;
+void	print_tokens_linked_list(t_token* head) {
+	t_token *temp;
 
-// 	temp = head;
-//     while (temp != NULL) {
-//         printf("%s\n", temp->token);
-//         temp = temp->next;
-//     }
-// }
+	temp = head;
+    while (temp != NULL) {
+        printf("token:%s\n", temp->token);
+        temp = temp->next;
+    }
+}
 
 int	main(void)
 {
 	t_token	*tokens;
 
-	tokens = NULL;
-	getting_line(tokens);
-	printf("Hello\n");
-	printf("%p\n", tokens);
+	tokens = getting_line();
+	print_tokens_linked_list(tokens);
 	return (0);
 }
