dc9b91d05d32be8902e0a9a1add5afacdf9975ca
maplepy <github.register@slmail.me>
2023-04-03 14:44:50 +0200

    add libft and unistd

--- header/minishell.h
+++ header/minishell.h
@@ -6,14 +6,16 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:55:43 by croy              #+#    #+#             */
-/*   Updated: 2023/04/02 20:15:58 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 14:39:42 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/header/libft.h"
 # include <stdio.h>
 # include <limits.h>
+# include <unistd.h>
 
 #endif
