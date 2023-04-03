cdc1d1224ac9a6c3fb9cf9c322a161d613626dd2
maplepy <github.register@slmail.me>
2023-04-03 17:09:02 +0200

    only include my libs

--- header/exec.h
+++ header/exec.h
@@ -6,17 +6,14 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:26:14 by croy              #+#    #+#             */
-/*   Updated: 2023/04/03 16:56:09 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/03 17:08:51 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
-# include "minishell.h"
-
 # include <limits.h>
 # include <unistd.h>
-// # include <stdio.h>
 
 #endif
