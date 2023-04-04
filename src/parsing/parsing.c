4f6fcc8442edbcb1f612e67344e554829fe9a8dc
Arthur <arthurascedusnkrs@gmail.com>
2023-04-04 14:18:05 +0200

    removed main in my files

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: arthurascedu <arthurascedu@student.42ly    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 10:19:54 by arthurasced       #+#    #+#             */
-/*   Updated: 2023/04/04 13:35:22 by arthurasced      ###   ########lyon.fr   */
+/*   Updated: 2023/04/04 14:17:45 by arthurasced      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@ void	print_tokens_linked_list(t_token *head)
 	}
 }
 
-int	main(int argc, char **argv, char **envp)
-{
-	// t_token		*tokens;
-	t_parsing	data;
-	t_list		*list;
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	// t_token		*tokens;
+// 	t_parsing	data;
+// 	t_list		*list;
 
-	data.begin = 0;
-	data.end = 0;
-	data.quote = 0;
-	data.d_quote = 0;
-	data.in_word = 0;
-	(void)argc;
-	(void)argv;
-	list = get_envp(envp);
-	print_list(list);
-	//tokens = getting_line(&data);
-	// print_tokens_linked_list(tokens);
-	return (0);
-}
+// 	data.begin = 0;
+// 	data.end = 0;
+// 	data.quote = 0;
+// 	data.d_quote = 0;
+// 	data.in_word = 0;
+// 	(void)argc;
+// 	(void)argv;
+// 	list = get_envp(envp);
+// 	print_list(list);
+// 	//tokens = getting_line(&data);
+// 	// print_tokens_linked_list(tokens);
+// 	return (0);
+// }
