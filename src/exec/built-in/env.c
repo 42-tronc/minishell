d5e9503c6418ebb1e9171b143cdf56263aa31fae
maplepy <github.register@slmail.me>
2023-04-08 00:21:34 +0200

    remove broken main

--- src/exec/built-in/env.c
+++ src/exec/built-in/env.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 17:23:26 by croy              #+#    #+#             */
-/*   Updated: 2023/04/06 17:42:37 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/06 18:06:01 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,18 @@ void	print_env(t_list *env)
 	}
 }
 
-/* int	main(void)
+int	main(void, void, char **envp)
 {
-	t_list	*list = malloc(sizeof(t_list));
+	// t_list	*list = malloc(sizeof(t_list));
 
-	list->content = 1;
-	list->next = malloc(sizeof(t_list));
-	list->next->content = 2;
-	list->next->next = NULL;
+	// list->content = 1;
+	// list->next = malloc(sizeof(t_list));
+	// list->next->content = 2;
+	// list->next->next = NULL;
 
-	// Print the contents of the linked list
-	print_env(list);
+	t_list	*env;
 
+	env = list_env(envp);
+	print_env(env);
 	return 0;
-} */
+}
