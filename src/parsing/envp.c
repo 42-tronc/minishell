098fff2c2b9ea19e467ea90397f83f2f83018457
Maplepy <github.register@slmail.me>
2023-04-08 17:44:42 +0200

    fix ft_setenv to do the same as the setenv function

--- src/parsing/envp.c
+++ src/parsing/envp.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 13:17:39 by arthurasced       #+#    #+#             */
-/*   Updated: 2023/04/08 16:50:19 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/08 17:44:22 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,39 @@ char	*ft_getenv(t_list *list_env, char *str)
 void	ft_setenv(t_list *list_envp, char *var, char *value)
 {
 	t_list	*temp;
+	char	*var_prefixed;
+
+	temp = list_envp;
+	// Add `=` to var
+	var_prefixed = malloc((ft_strlen(var) + 2) * sizeof(char));
+	if (!var_prefixed)
+		return; // to change if malloc fails
+	ft_strlcpy(var_prefixed, var, ft_strlen(var) + 2);
+	ft_strlcat(var_prefixed, "=", ft_strlen(var) + 2);
+	// printf("old var: `%s`\nnew var: `%s`\n", var, var_prefixed);
+
+	while (temp->next != NULL)
+	{
+		if (ft_strncmp(temp->content, var, ft_strlen(var)) == 0)
+		{
+			free(temp->content);
+			temp->content = ft_strjoin(var_prefixed, value);
+			free(var_prefixed);
+			return ;
+		}
+		temp = temp->next;
+	}
+	ft_lstadd_back(&list_envp, ft_lstnew(ft_strjoin(var_prefixed, value)));
+}
+
+/*
+/// @brief Change or add back a variable in our linked list of envp.
+/// @param list_envp Linked list of envp
+/// @param var Name of the variable with '=' at the end (IMPORTANT TO ADD =)
+/// @param value The value to be replace or added to the linked list
+void	ft_setenv(t_list *list_envp, char *var, char *value)
+{
+	t_list	*temp;
 
 	temp = list_envp;
 	while (temp->next != NULL)
@@ -85,4 +118,4 @@ void	ft_setenv(t_list *list_envp, char *var, char *value)
 		temp = temp->next;
 	}
 	ft_lstadd_back(&list_envp, ft_lstnew(ft_strjoin(var, value)));
-}
+} */
