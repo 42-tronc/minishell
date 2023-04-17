780011348c13bc329472fa523b7ef321b614ad71
maple <28269333+maplepy@users.noreply.github.com>
2023-04-17 08:13:02 +0000

    Update export.c

--- src/exec/built-in/export.c
+++ src/exec/built-in/export.c
@@ -21,6 +21,33 @@ EXPORT
 could do with an index,
  */
 
+void ft_sort_env(t_list *env)
+{
+    t_list *current;
+    t_list *next;
+    char *tmp;
+
+    if (env == NULL)
+        return;
+    current = env;
+    while (current->next)
+    {
+        next = current->next;
+        while (next)
+        {
+            if (strcmp(((char *)current->content), ((char *)next->content)) > 0)
+            {
+                tmp = current->content;
+                current->content = next->content;
+                next->content = tmp;
+            }
+            next = next->next;
+        }
+        current = current->next;
+    }
+}
+
+
 int	main(int ac, char **av, char **envp)
 {
 	(void) ac;
