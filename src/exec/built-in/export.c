1659d9e7ec327dbc64350e1897e26846e431b5ac
Maplepy <croy@student.42lyon.fr>
2023-04-18 17:48:41 +0200

    add setexport

--- src/exec/built-in/export.c
+++ src/exec/built-in/export.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 16:35:26 by croy              #+#    #+#             */
-/*   Updated: 2023/04/18 15:36:43 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/18 16:35:55 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ EXPORT
 - prefix with 'declare -x '
 
 export LOL _ww
-	will add 'LOL' AND '_ww' in export
+	'LOL'
+	'_ww'
 export LOL="ww"	_ww="sadge"
-	will add and set 'LOL="ww"' AND '_ww="sadge"' in export
+	'LOL="ww"'
+	'_ww="sadge"'
+export LOL==poopi
+	LOL="=poopi"
 
 
  */
@@ -52,10 +56,30 @@ void ft_sort_env(t_list *env)
 	}
 }
 
+void	ft_setexport(t_list	*export, char *str)
+{
+	// check if `=` or not
+	int	i;
 
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+		{
+			ft_setenv(export, )
+		}
+		i++;
+	}
+
+}
 
 void	ft_export(t_list *env)
 {
+	// OPERATIONS GO HERE BEFORE SORTING
+	// si export plusieurs mots
+
+
+
 	printf("\n\n\n\n\tEXPORT HERE\n\n");
 	ft_sort_env(env);
 	while (env)
@@ -76,8 +100,8 @@ int	main(int ac, char **av, char **envp)
 	env = list_env(envp);
 	// export = list_env(envp);
 	ft_setenv(env, "MY_VAR", "world");
-	printf("\n\n\n\tOG\n\n");
-	ft_env(env);
+	// printf("\n\n\n\tOG\n\n");
+	// ft_env(env);
 	// ft_export(export);
 	// printf("\n\n\n\tOG AGAIN\n\n");
 	// ft_env(env);
