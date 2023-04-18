f0d10bb7c4fcb768b9ecd5ffb19266eef0a4bd4a
Maplepy <croy@student.42lyon.fr>
2023-04-18 15:37:38 +0200

    add more todo and use ft_export to do everything (working on splitting var and value)

--- src/exec/built-in/export.c
+++ src/exec/built-in/export.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 16:35:26 by croy              #+#    #+#             */
-/*   Updated: 2023/04/17 14:03:19 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/18 15:36:43 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@ EXPORT
 - sort var by alpha
 - prefix with 'declare -x '
 
-could do with an index,
+export LOL _ww
+	will add 'LOL' AND '_ww' in export
+export LOL="ww"	_ww="sadge"
+	will add and set 'LOL="ww"' AND '_ww="sadge"' in export
+
+
  */
 
 void ft_sort_env(t_list *env)
@@ -27,7 +32,7 @@ void ft_sort_env(t_list *env)
 	t_list	*next;
 	char	*tmp;
 
-	if (env == NULL)
+	if (!env)
 		return;
 	current = env;
 	while (current->next)
@@ -47,12 +52,16 @@ void ft_sort_env(t_list *env)
 	}
 }
 
+
+
 void	ft_export(t_list *env)
 {
 	printf("\n\n\n\n\tEXPORT HERE\n\n");
+	ft_sort_env(env);
 	while (env)
 	{
 		printf("declare -x %s\n", (char *)env->content);
+		// printf("declare -x %s=\"%s\"\n", (char *)env->var, var->value);
 		env = env->next;
 	}
 }
@@ -61,14 +70,17 @@ int	main(int ac, char **av, char **envp)
 {
 	(void) ac;
 	(void) av;
-
 	t_list	*env;
+	// t_list	*export;
+
 	env = list_env(envp);
+	// export = list_env(envp);
 	ft_setenv(env, "MY_VAR", "world");
+	printf("\n\n\n\tOG\n\n");
 	ft_env(env);
-	ft_export(env);
-	printf("\n\n\nCIAO BELLI BELLA\n\n\n\n");
-	ft_env(env);
+	// ft_export(export);
+	// printf("\n\n\n\tOG AGAIN\n\n");
+	// ft_env(env);
 
 	return (0);
 }
