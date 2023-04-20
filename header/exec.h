182f71c72949335f6a89650b161cfd059bfbdf0c
Maplepy <github.register@slmail.me>
2023-04-20 13:57:25 +0200

    revert exec to previous version

--- header/exec.h
+++ header/exec.h
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:26:14 by croy              #+#    #+#             */
-/*   Updated: 2023/04/20 13:54:12 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/20 13:56:08 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@
 
 # define RED	"\e[31;1m"
 # define RESET	"\e[0m"
+
+typedef struct s_env
+{
+	char			*var;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
 
 // char	*ft_pwd(void);
 void	ft_cd(char *path, t_list *env);
