2e016361b99c6bf5cf48eacf7ee86939e9b99e6c
Maplepy <github.register@slmail.me>
2023-04-20 16:54:10 +0200

    add todo in exit

--- src/exec/built-in/exit.c
+++ src/exec/built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:08:49 by croy              #+#    #+#             */
-/*   Updated: 2023/04/05 17:48:09 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/20 16:34:04 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 NEED
 
 free everything then exit with proper exit code
+check if nb
 need to divide something by something to get real exit code
  */
 int	ft_exit(int code);
