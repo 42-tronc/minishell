3755f875e4fc2856d59984145d2c79447f7af889
Maplepy <github.register@slmail.me>
2023-04-24 12:17:48 +0200

    change cd proto and reorder protos

--- header/exec.h
+++ header/exec.h
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:26:14 by croy              #+#    #+#             */
-/*   Updated: 2023/04/24 12:15:32 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/24 12:17:34 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,17 @@ typedef struct s_env
 	struct s_env	*next;
 }					t_env;
 
-// char	*ft_pwd(void);
-void	ft_cd(char *path, t_list *env);
+// void	ft_cd(char *path, t_list *env);
+void	ft_cd(t_env *env, char *path);
+
 void	ft_echo(int ac, char **av);
 void	ft_env(t_list *env);
 void	ft_export(t_list *env);
+
+// char	*ft_pwd(void);
 int		ft_pwd(void);
 
+// Utils for env
 t_env	*fill_env(char **envp);
 char	*ft_getenv(t_env *ll_env, char *var);
 int		ft_setenv(t_env *ll_env, char *var, char *replace);
