6d4210de898d45027900c5e2186f03a42f70b565
Maplepy <github.register@slmail.me>
2023-04-24 12:04:10 +0200

    fix next not being set with env add back and env new

--- src/exec/built-in/utils.c
+++ src/exec/built-in/utils.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 11:38:39 by croy              #+#    #+#             */
-/*   Updated: 2023/04/20 14:09:32 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/04/24 12:03:39 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,123 @@
 /*
 NEED
 
+	strtok
 	Fill env to linked list
 	Get value from var in linked list
 	Set value from var in linked list
  */
 
-void	fill_env(char **envp)
+// char *ft_strtok(char *str, char *delim);
+
+t_env	*ft_env_new(char *var, char *value)
 {
-	int	i;
+	t_env	*dst;
+
+	dst = malloc(sizeof(t_env));
+	if (!dst)
+		return (NULL);
+	dst->var = var;
+	dst->value = value;
+	dst->next = NULL;
+	return (dst);
+}
+
+void	ft_env_add_back(t_env **lst, t_env *new)
+{
+	t_env	*temp;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
+
+/**
+ * @brief gets the envp from a main function and fills a t_env linked list
+ * will set the variable in `var` and value in `value`
+ *
+ * @param envp from a main function where (char **envp)
+ * @return t_env* the linked list with every var and its value from the env
+ */
+t_env	*fill_env(char **envp)
+{
+	int		i;
+	int		j;
+	t_env	*ll_env;
+	t_env	*current;
+
+	i = -1;
+	ll_env = NULL;
+	while (envp[++i])
+	{
+		j = -1;
+		while (envp[i][++j])
+		{
+			if (envp[i][j] == '=')
+			{
+				// printf("%c here \n", envp[i][j]);
+				printf("\n`%s`\n", envp[i]);
+				envp[i][j] = '\0';
+				current = ft_env_new(ft_strdup(envp[i]), ft_strdup(envp[i] + j + 1));
+				printf("`%s`", current->var);
+				printf("`%s`\n", current->value);
+
+				if (!current)
+					return (NULL);
+					// free memory here
+				ft_env_add_back(&ll_env, current);
+				break;
+			}
+			// j++;
+		}
+		// i++;
+	}
+	return (ll_env);
+}
+
+/* t_env	*fill_env(char **envp)
+{
+	int		i;
+	int		j;
+	t_env	*start;
+	t_env	*temp;
+	t_env	*ll_env;
 
 	i = 0;
-	// t_env	*env;
-
+	j = 0;
+	// ll_env= ft_calloc(sizeof(t_env), 1);
+	// if (!ll_env)
+	// 	return (NULL);
+	ll_env = NULL;
 	while (envp[i])
-		printf("%s\n", envp[i++]);
-}
+	{
+		j = 0;
+		printf("\n%s\n", envp[i]);
+		while (envp[i][j])
+		{
+			if (envp[i][j] == '=')
+			{
+				// printf("%c here \n", envp[i][j]);
+				envp[i][j] = '\0';
+				ll_env->var = ft_strdup(envp[i]);
+				printf("var\t`%s`\n", ll_env->var);
+
+				envp[i] += j + 1;
+				ll_env->value = ft_strdup(envp[i]);
+				printf("value\t`%s`\n", ll_env->value);
+				// ll_env->next =
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (ll_env);
+} */
