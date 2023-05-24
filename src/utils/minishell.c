3e5ae6c2cbf232ec6aabbc46eaef68f53a67b3cb
maplepy <github.register@slmail.me>
2023-05-24 12:51:38 +0200

    add: protection in case init data fails

--- src/utils/minishell.c
+++ src/utils/minishell.c
@@ -216,7 +216,7 @@ int	main(int argc, char **argv, char **envp)
 			id_tokens(&data->tokens);
 			// print_tokens_linked_list(data->tokens);
 
-			if (!init_data(data))
+			if (init_data(data))
 				exit(FAILURE);
 
 			exec_dispatch(data, data->tokens);
