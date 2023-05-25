5908331f7af847861bacd516e83a6f00479a4c7c
Arthur <arthurascedusnkrs@gmail.com>
2023-05-25 12:42:42 +0200

    c'est le j c'est le s 1 3 mathafack

--- src/parsing/prepare.c
+++ src/parsing/prepare.c
@@ -6,5 +6,5 @@ void	prepare_token(t_data *data)
 	expand_tilde(&data->tokens, data);
 	remove_quotes(&data->tokens, data);
 	id_tokens(&data->tokens);
-	print_tokens_linked_list(data->tokens);
+	// print_tokens_linked_list(data->tokens);
 }
