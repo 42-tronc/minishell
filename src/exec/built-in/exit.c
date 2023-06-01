525ef0a9e6b38c2a9f61b7d76d048014bd45b481
maplepy <28269333+maplepy@users.noreply.github.com>
2023-06-01 11:46:18 +0000

    🎉 add: exit, almost perfect (need to check if above long)

--- src/exec/built-in/exit.c
+++ src/exec/built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:08:49 by croy              #+#    #+#             */
-/*   Updated: 2023/06/01 09:54:26 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/01 11:44:00 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,59 @@ need to divide something by something to get real exit code
  */
 #include "minishell.h"
 
+static int	is_int(long long number)
+{
+	return (number >= LONG_MIN && number <= 9223372036854775807);
+}
+
+static int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	// skip leading whitespace
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	// check for sign
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	// check for digits
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		i++;
+	}
+	// skip trailing whitespace
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	// check if the string is terminated
+	printf("is valid number %d\n", !str[i]);
+	return (str[i] == '\0');
+}
+
 void	ft_exit(t_token *input)
 {
-	int	exit_code;
+	long long	exit_code;
 
-	printf("error code %s\n", input->token);
 	if (!input)
 		exit(0);
-	exit_code = ft_atoi(input->token);
-	while (exit_code < 0)
+	// checkString(input->token, 0, 0, 1);
+	if (is_valid_number(input->token))
 	{
-		exit_code = exit_code + 256;
+		exit_code = atoll(input->token);
+		printf("error code %lld\n", exit_code);
+		if (!is_int(exit_code))
+		{
+			printf("exit: numeric argument required\n");
+			exit(2);
+		}
+		while (exit_code < 0)
+			exit_code = exit_code + 256;
+		exit_code = exit_code % 256;
+		exit(exit_code);
 	}
-	exit_code = exit_code % 256;
-	exit(exit_code);
+	else
+	{
+		printf("exit: numeric argument required\n");
+		exit(2);
+	}
 }
