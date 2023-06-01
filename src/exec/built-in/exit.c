54611a87092bfc740aa19b6385538a233d6c2ba8
maplepy <28269333+maplepy@users.noreply.github.com>
2023-06-01 09:54:50 +0000

    🔄 update: working on exit

--- src/exec/built-in/exit.c
+++ src/exec/built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:08:49 by croy              #+#    #+#             */
-/*   Updated: 2023/04/20 16:34:04 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/01 09:54:26 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,20 @@ free everything then exit with proper exit code
 check if nb
 need to divide something by something to get real exit code
  */
-int	ft_exit(int code);
+#include "minishell.h"
+
+void	ft_exit(t_token *input)
 {
-	return (exit(code), code);
+	int	exit_code;
+
+	printf("error code %s\n", input->token);
+	if (!input)
+		exit(0);
+	exit_code = ft_atoi(input->token);
+	while (exit_code < 0)
+	{
+		exit_code = exit_code + 256;
+	}
+	exit_code = exit_code % 256;
+	exit(exit_code);
 }
