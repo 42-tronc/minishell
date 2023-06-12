58362a7e45352c992bf211309ca3889fdc5f5d92
maplepy <github.register@slmail.me>
2023-06-12 10:41:21 +0200

    🔄 update: cut cd into two parts for the norm

--- src/exec/built-in/cd.c
+++ src/exec/built-in/cd.c
@@ -3,23 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maplepy <maplepy@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 15:22:58 by croy              #+#    #+#             */
-/*   Updated: 2023/04/08 16:30:07 by croy             ###   ########lyon.fr   */
-
+/*   Updated: 2023/06/12 10:25:33 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-NEED
-`cd -`	to go back in time
-`cd ~`	to go home
-`cd ~~`	to go home
+static char	*get_cd_path(t_data *data, t_token *input)
+{
+	char	*path;
 
-*/
+	// HOME Directory
+	if (!input || ft_strcmp(input->token, "~") == 0)
+		path = ft_getenv(data->env, "HOME");
+
+	// LAST Directory
+	else if (ft_strcmp(input->token, "-") == 0)
+	{
+		if (ft_getenv(data->env, "OLDPWD"))
+		{
+			path = ft_getenv(data->env, "OLDPWD");
+		}
+		else
+		{
+			printf("cd: OLDPWD not set\n");
+			return (NULL);
+		}
+	}
+	else
+		path = input->token;
+	return (path);
+}
 
 /**
  * @brief Goes into the directory specified (works with `~`)
@@ -40,41 +57,19 @@ void	ft_cd(t_data *data, t_token *input)
 
 	// Get the current dir to make it the OLDPWD
 	getcwd(previous, BUFSIZ);
-	// printf("previous\t`%s`\n", previous);
-	// printf("oldpwd\t\t`%s`\n", ft_getenv(data->env, "OLDPWD"));
+	path = get_cd_path(data, input);
+	printf("path = `%s`\n", path);
 
-	// HOME Directory
-	if (!input || ft_strcmp(input->token, "~") == 0)
-		path = ft_getenv(data->env, "HOME");
-
-	// LAST Directory
-	else if (ft_strcmp(input->token, "-") == 0)
-	{
-		if (ft_getenv(data->env, "OLDPWD"))
-		{
-			path = ft_getenv(data->env, "OLDPWD");
-			// printf("found OLDPWD\t`%s`\n", path);
-			// path = getenv("OLDPWD");
-		}
-		else
-		{
-			// printf(RED"cd: OLDPWD not set: '%s'\n"RESET, ft_getenv(env, "OLDPWD"));
-			printf("cd: OLDPWD not set\n");
-			return;
-		}
-	}
-	else
-		path = input->token;
-
-	// CHANGE Directory
-	// printf("path\t\t`%s`\n", path);
+	if (!path)
+		return;
 	if (chdir(path) == -1)
 		perror("cd");
 	else
 	{
 		ft_setenv(data->env, "OLDPWD", previous);
-		printf("New OLDPWD\t`%s`\n", ft_getenv(data->env, "OLDPWD"));
+		printf("New OLDPWD\t%s\n", ft_getenv(data->env, "OLDPWD"));
 	}
+	printf("New path\t");
 	ft_pwd();
 	// printf("Moved to %s\n", path);
 }
