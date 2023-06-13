993b0c118947b9235d35c1f2d5b803740e3464a4
maple <28269333+maplepy@users.noreply.github.com>
2023-06-13 08:58:44 +0000

    🔄 update: move fill_env to env.c

--- src/exec/built-in/env.c
+++ src/exec/built-in/env.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 17:23:26 by croy              #+#    #+#             */
-/*   Updated: 2023/06/09 11:09:18 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/13 08:58:24 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,45 @@ void	print_env(t_data *data, t_token *input, int block)
 void	ft_env(t_data *data, t_token *input, int block)
 {
 	create_subshell(print_env, data, input, block);
+}
+
+/**
+ * @brief gets the envp from a main function and fills a t_env linked list
+ * will set the variable in `var` and value in `value`
+ *
+ * @param envp from a main function where (char **envp)
+ * @return t_env* the linked list with every var and its value from the env
+ */
+t_env	*fill_env(char **envp)
+{
+	int		i;
+	int		j;
+	t_env	*ll_env;
+	t_env	*current;
+
+	i = -1;
+	ll_env = NULL;
+	while (envp[++i])
+	{
+		j = -1;
+		while (envp[i][++j])
+		{
+			if (envp[i][j] == '=')
+			{
+				envp[i][j] = '\0';
+				current = ft_env_new(envp[i], envp[i] + j + 1);
+				// printf("`%s`", current->var);
+				// printf("`%s`\n", current->value);
+
+				if (!current)
+					return (NULL);
+					// free memory here
+				ft_env_add_back(&ll_env, current);
+				break;
+			}
+			// j++;
+		}
+		// i++;
+	}
+	return (ll_env);
 }
