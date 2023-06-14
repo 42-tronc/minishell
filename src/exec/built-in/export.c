7827c691fb93c66589df2bb95964b4d28ce6411f
maple <28269333+maplepy@users.noreply.github.com>
2023-06-14 11:41:10 +0000

    🔄 update: make empty exports red for now

--- src/exec/built-in/export.c
+++ src/exec/built-in/export.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 16:35:26 by croy              #+#    #+#             */
-/*   Updated: 2023/06/14 10:57:53 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/14 11:40:52 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	add_env_entry(t_env *env, t_token *input, int block)
 				value++;
 			}
 			// else
-			printf("var = `%s` | value = `%s`\n", var, value);
+			// printf("var = `%s` | value = `%s`\n", var, value);
 			ft_setenv(env, var, value);
 			printf("getenv %s = `%s`\n", var, ft_getenv(env, var));
 		}
@@ -85,7 +85,7 @@ static void	export_print(t_data *data, t_token *input, int block)
 		if (data->env->value)
 			printf("declare -x %s=\"%s\"\n", data->env->var, data->env->value);
 		else
-			printf("declare -x %s\n", data->env->var);
+			printf(RED "declare -x %s%s\n", data->env->var, RESET);
 		data->env = data->env->next;
 	}
 }
