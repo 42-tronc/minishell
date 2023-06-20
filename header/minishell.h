08bfd746b8d0f119853242c411d04bacc67b17fc
maplepy <croy@student.42lyon.fr>
2023-06-20 15:35:31 +0200

    fix export

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:55:43 by croy              #+#    #+#             */
-/*   Updated: 2023/06/19 17:19:50 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/20 14:11:05 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,8 +149,8 @@ long	long	ft_atoll(const char *str);
 int	ft_exit(t_data *data, t_token *input, int block);
 
 // export.c
-void	add_env_entry(t_env *env, t_token *input, int block);
-void	ft_export(t_data *data, t_token *input, int block);
+int	add_env_entry(t_env *env, t_token *input, int block);
+int	ft_export(t_data *data, t_token *input, int block);
 
 // pwd.c
 int	ft_pwd(t_data *data, t_token *input, int block);
