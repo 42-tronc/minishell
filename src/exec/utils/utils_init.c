beac9def682c3a93f6a5506a7e71bff0778c188b
maple <github.register@slmail.me>
2023-06-25 14:29:35 +0200

    🎉 add: default env values

--- src/exec/utils/utils_init.c
+++ src/exec/utils/utils_init.c
@@ -6,11 +6,21 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 20:57:10 by croy              #+#    #+#             */
-/*   Updated: 2023/06/24 20:57:14 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/25 14:20:37 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	fill_default_env(t_data *data)
+{
+	ft_setenv(&(data->env), "OLDPWD", "");
+	if (!ft_getenv(data->env, "SHLVL"))
+		ft_setenv(&(data->env), "SHLVL", "1");
+	else
+		ft_setenv(&(data->env), "SHLVL", ft_itoa(ft_atoi(ft_getenv(data->env, "SHLVL")) + 1));
+	ft_setenv(&(data->env), "PWD", getcwd(NULL, 0));
+}
 
 /**
  * @brief gets the envp from a main function and fills a t_env linked list
@@ -45,4 +55,5 @@ void	fill_env(t_data *data, char **envp)
 		}
 		i++;
 	}
+	fill_default_env(data);
 }
