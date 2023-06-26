d4ddd152976d004f1877abc2d1937431ac8f6e85
maple <28269333+maplepy@users.noreply.github.com>
2023-06-26 12:49:54 +0000

    🐛 fix: SHLVL too high

--- src/exec/utils/utils_init.c
+++ src/exec/utils/utils_init.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 20:57:10 by croy              #+#    #+#             */
-/*   Updated: 2023/06/26 12:32:40 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/26 12:49:00 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ static void	fill_default_env(t_data *data)
 	char	*shlvl;
 	char	*cwd;
 
-
 	ft_setenv(&(data->env), "OLDPWD", NULL);
 	if (!ft_getenv(data->env, "SHLVL"))
 		ft_setenv(&(data->env), "SHLVL", "1");
+	else if (ft_atoi(ft_getenv(data->env, "SHLVL")) >= 999)
+	{
+		write(2, "bash: warning: shell level too high, resetting to 1\n", 52);
+		ft_setenv(&(data->env), "SHLVL", "1");
+	}
 	else
 	{
 		shlvl = ft_itoa(ft_atoi(ft_getenv(data->env, "SHLVL")) + 1);
