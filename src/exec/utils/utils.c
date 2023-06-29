7fdf6938eec3287cbca6df15ecad62a285effd04
maple <28269333+maplepy@users.noreply.github.com>
2023-06-29 07:55:12 +0000

    🔄 update: move subshell utils to their own file

--- src/exec/utils/utils.c
+++ src/exec/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 11:38:39 by croy              #+#    #+#             */
-/*   Updated: 2023/06/29 07:52:20 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/06/29 07:54:39 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,56 +30,6 @@ void	exit_error(int code, char *source)
 	exit(EXIT_FAILURE);
 }
 
-void	close_all_pipes(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->cmd_ct)
-	{
-		if (data->cmd_block[i]->pipe_fd[0] > 0)
-		{
-			close(data->cmd_block[i]->pipe_fd[0]);
-			data->cmd_block[i]->pipe_fd[0] = -1;
-		}
-		if (data->cmd_block[i]->pipe_fd[1] > 0)
-		{
-			close(data->cmd_block[i]->pipe_fd[1]);
-			data->cmd_block[i]->pipe_fd[1] = -1;
-		}
-		i++;
-	}
-}
-
-void	create_subshell(int (*func)(t_data*, t_token*, int), t_data *data, t_token *input, int block)
-{
-	pid_t	pid;
-	int		status;
-
-	status = 0;
-	pid = fork();
-	if (pid == -1)
-		exit_error(E_FORK, "create_subshell");
-	else if (pid == 0)
-	{
-		if (block < data->cmd_ct - 1 && data->cmd_block[block]->pipe_fd[0] > 0)
-			close(data->cmd_block[block]->pipe_fd[0]);
-		check_input(data, block);
-		check_output(data, block);
-		close_all_pipes(data);
-		status = func(data, input, block);
-		exit(status);
-	}
-	else
-	{
-		data->cmd_block[block]->pid = pid;
-		if (block > 0 && data->cmd_block[block - 1]->pipe_fd[0] > 0)
-			close(data->cmd_block[block - 1]->pipe_fd[0]);
-		if (data->cmd_block[block]->pipe_fd[1] > 0 && block < data->cmd_ct - 1)
-			close(data->cmd_block[block]->pipe_fd[1]);
-	}
-}
-
 /**
  * @brief Counts the number of arguments in the input
  * @remark The command is not counted as an argument
@@ -90,7 +40,7 @@ void	create_subshell(int (*func)(t_data*, t_token*, int), t_data *data, t_token 
  */
 int	count_arguments(t_token *input)
 {
-	int		arg_count;
+	int	arg_count;
 
 	arg_count = 0;
 	while (input)
@@ -117,16 +67,4 @@ void	swap_var(char **current, char **next)
 	tmp = *current;
 	*current = *next;
 	*next = tmp;
-}
-
-int	check_alone(int (*func)(t_data*, t_token*, int), t_data *data, t_token *input, int block)
-{
-	int	status;
-
-	status = 0;
-	if (data->cmd_ct == 1)
-		status = func(data, input, block);
-	else
-		create_subshell(func, data, input, block);
-	return (status);
 }
