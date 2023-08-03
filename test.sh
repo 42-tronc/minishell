0fcb78fa7f5413f1da68de2b25e4d324773f1fda
maple <github.register@slmail.me>
2023-08-03 14:30:40 +0200

    🔄 update: more thorough tests

--- test.sh
+++ test.sh
@@ -3,19 +3,26 @@
 # Define the folder structure
 folder_structure="a/b/c/"
 
+# Wait for 1 second
+sleep 1
+
 # Create the folder structure
 mkdir -p "$folder_structure"
 
 # Navigate to the last folder in the structure using xdotool
-sleep 1
 xdotool type "cd $(realpath "$folder_structure")"
 xdotool key Return
-sleep 1
+sleep 0.5
 
 # Type pwd using xdotool
 xdotool type "pwd"
 xdotool key Return
 sleep 0.5
+
+# Change the Home
+# xdotool type "export HOME=\"$(realpath "$folder_structure")\""
+# xdotool key Return
+# sleep 0.5
 
 # Remove the first folder and its subfolders
 rm -rf "$(realpath "${folder_structure%%/*}")"
@@ -28,3 +35,29 @@ sleep 0.5
 # Navigate back using cd ..
 xdotool type "cd .."
 xdotool key Return
+sleep 0.5
+
+# Type pwd using xdotool
+xdotool type "pwd"
+xdotool key Return
+sleep 0.5
+
+# Type echo $OLDPWD
+xdotool type "echo \$OLDPWD"
+xdotool key Return
+sleep 0.5
+
+# Type cd
+xdotool type "cd"
+xdotool key Return
+sleep 0.5
+
+# Type pwd using xdotool
+xdotool type "pwd"
+xdotool key Return
+sleep 0.5
+
+# Type echo $OLDPWD
+xdotool type "echo \$OLDPWD"
+xdotool key Return
+sleep 0.5
