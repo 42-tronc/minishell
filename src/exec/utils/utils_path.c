56fbd0df383194d10b8e153a0574360e7c8f269f
maple <github.register@slmail.me>
2023-08-11 11:41:37 +0200

    🔄 update: make utils_path more robust and split get_validpath

--- src/exec/utils/utils_path.c
+++ src/exec/utils/utils_path.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:31:15 by croy              #+#    #+#             */
-/*   Updated: 2023/08/10 09:34:26 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/08/11 11:40:13 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,26 @@ static int	ft_getpaths(t_data *data)
 	return (0);
 }
 
+static char	*find_path(t_data *data, char *token)
+{
+	int		i;
+	char	*command_path;
+
+	i = 0;
+	while (data->paths[i])
+	{
+		(void) token;
+		command_path = ft_strjoin(data->paths[i], token);
+		if (!command_path)
+			return (print_error(E_MALLOC, "find_path"), NULL);
+		if (access(command_path, F_OK) == 0)
+			return (command_path);
+		free(command_path);
+		i++;
+	}
+	return (NULL);
+}
+
 /**
  * @brief Gets the path of the command passed as input
  *
@@ -36,12 +56,14 @@ static int	ft_getpaths(t_data *data)
  */
 char	*get_validpath(t_data *data, t_token *input, char **env_array)
 {
-	int		i;
 	char	*command_path;
 
-	i = -1;
+	if (!input || !input->token)
+		return (NULL);
 	command_path = ft_strdup(input->token);
-	if (!input || !command_path || !ft_strcmp(command_path, ""))
+	if (!command_path)
+		return (print_error(E_MALLOC, "get_validpath"), NULL);
+	if (!ft_strcmp(command_path, ""))
 		return (free(command_path), NULL);
 	if (ft_strchr(input->token, '/'))
 		return (command_path);
@@ -51,14 +73,6 @@ char	*get_validpath(t_data *data, t_token *input, char **env_array)
 		return (free(command_path), \
 		free_array(env_array), clean_exit(data, E_MALLOC, "ft_getpaths"), NULL);
 	free(command_path);
-	while (data->paths[++i])
-	{
-		command_path = ft_strjoin(data->paths[i], input->token);
-		if (!command_path)
-			return (NULL);
-		if (access(command_path, F_OK) == 0)
-			return (command_path);
-		free(command_path);
-	}
-	return (NULL);
+	command_path = find_path(data, input->token);
+	return (command_path);
 }
